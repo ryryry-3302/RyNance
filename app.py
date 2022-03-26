e5f134a0530f9252e66277086c5ef34a260f5a0d
Ryan Leong <rjleong2002@gmail.com>
2022-03-26 15:44:07 +0800

    Added password confirmation and validation

--- app.py
+++ app.py
@@ -35,17 +35,59 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
+
 @app.route("/")
 @login_required
 def index():
     return render_template("index.html")
+
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     session.clear()
 
     if request.method == "GET":
+        return render_template("login.html")
+
+    
+    return apology("u fucked up", 400)
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "GET":
         return render_template("register.html")
 
-    else:
-        return apology("u fucked up", 400)
+    """Register user"""
+    name = request.form.get("UserName")
+    password = generate_password_hash(request.form.get("password"))
+
+    if not request.form.get("username"):
+        return apology("must provide username", 400)
+
+        # Ensure password was submitted
+    elif not request.form.get("password"):
+        return apology("must provide password", 400)
+
+    if request.form.get("password") != request.form.get("confirmation"):
+        return apology("Passwords dont match pepega")
+
+    if len(db.execute("SELECT username FROM users WHERE username = ?", username)) > 0:
+        return apology("Username taken lol")
+
+    db.execute('INSERT INTO users (username, hash) values(?, ?)', username, password)
+    return redirect("/login")
+
+    
+
+    
