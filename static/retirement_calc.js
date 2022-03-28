0e268986be108762ccf42669dc699f1bf4336b1f
Ryan Leong <rjleong2002@gmail.com>
2022-03-29 02:44:28 +0800

    finished the js for retirement calc

--- static/retirement_calc.js
+++ static/retirement_calc.js
@@ -7,34 +7,38 @@ $(document).ready(function(){
         let age = parseInt($('#age').val());
         let port = parseInt($('#portvalue').val());
         let contr = parseInt($('#contribution').val());
-        let incr = parseFloat($('#incrementrate').val()/100).toFixed(2);
-        let withrate = parseFloat($('#Withdrawlrate').val()/100).toFixed(2);
+        let incr = Number(parseFloat($('#incrementrate').val()/100).toFixed(2));
+        let withrate = Number(parseFloat($('#Withdrawlrate').val()/100).toFixed(2));
         let withd = parseInt($('#withd').val());
-        let growth = parseFloat($('#growthrate').val()/100).toFixed(2);
-        let infl = parseFloat($('#inflationrate').val()/100).toFixed(2);
-        
+        let growth = Number(parseFloat($('#growthrate').val()/100).toFixed(2));
+        let infl = Number(parseFloat($('#inflationrate').val()/100).toFixed(2));
         let retirement_sum = withd / withrate;
         let real_growth = growth - infl + 1;
+        let incrate = (1 + incr);
+    
 
         while (age < 65) {
             age++;
-            port += contr;
-            contr *= (1 + incr);
-            port *= real_growth;
+            port = contr + port;
+    
+            contr = incrate * contr;
+            port = port * real_growth;
             if (port < retirement_sum) {
                 continue;
+
             }   else {
-                alert(age);
                 break;
             }
 
         }
+        if (age > 65) {
+        alert("cannot retire before 65");
+        } 
         
-        setTimeout(function(){
-            if (age > 65) {
-            alert("cannot retire before 65");
-            } 
-        }, 5000);
+        $('#retirementage').text(age);
+        $('#retireport').text('$' + port.toFixed(2));
+
+        
 
 
     }
